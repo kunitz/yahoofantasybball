fe0459816632e036f32cf8e188acb1f80f0a3875
lkunitz <logman@gmail.com>
2021-01-06 16:44:08 -0600

    Updated test cases to consolidate overall test class.

--- tests/test_fantasybasketballnerd.py
+++ tests/test_fantasybasketballnerd.py
@@ -2,28 +2,22 @@ from unittest import TestCase
 from yahoofantasybball.fantasybasketballnerd import FantastyBasketballNerd
 
 
-class TestFantastBasketballNerd(TestCase):
+class TestFantastyBasketballNerd(TestCase):
     def test_read_url(self):
         bball = FantastyBasketballNerd()
-        bball.readPlayers()
+        #bball.readPlayers()
 
-
-class TestFantastBasketballNerd(TestCase):
     def test_read_team_schedule(self):
         bball = FantastyBasketballNerd()
         bball.readTeamSchedule(bball.players["LeBron James"].team)
         print(bball.players["LeBron James"].team)
         print("done")
 
-
-class TestFantastBasketballNerd(TestCase):
     def test_week_start(self):
         bball = FantastyBasketballNerd()
         weekstart, weekend = bball.weekStartEnd(1)
         print(str(weekstart) + "  " + str(weekend))
 
-
-class TestFantastBasketballNerd(TestCase):
     def test_number_player_games_week(self):
         bball = FantastyBasketballNerd()
         for i in range(10):
